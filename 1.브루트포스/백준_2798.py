b312496bb638bdefb9a42b0e7fb628b3192b954e
jiyoon baek <67775336+jiyoonbaekbaek@users.noreply.github.com>
2021-10-10 14:00:53 +0900

    Create 백준_2798.py

---
+++ 1.브루트포스/백준_2798.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+card = list(map(int, input().split()))
+ans = 0
+for i in range(0, n-2):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            if ans < card[i]+card[j]+card[k] <= m:
+                ans = card[i]+card[j]+card[k]
+print(ans)
